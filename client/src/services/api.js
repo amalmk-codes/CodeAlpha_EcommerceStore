94d666339dae00c9c2f9f583d7948ff19720832e
amalmk-codes <amalmkcoder0028@gmail.com>
2026-06-17 16:14:29 +0530

    Connect frontend to Render backend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL:
+  "https://codealpha-store-backend.onrender.com/api",
 });
 
 // Automatically attach JWT token
